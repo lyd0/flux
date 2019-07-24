f2d87c74f923923a38c32bbdcc2e6958f4253f86
lyd0 <1170507875@qq.com>
2019-07-24 16:01:49 +0800

    test

---
+++ src/main/java/com/example/fluxdemo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.fluxdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Mono<String> hello() {
+        return Mono.just("Welcome to reactive world ~");
+    }
+    @GetMapping("/")
+    public String index(){
+        return "idnex";
+    }
+}
